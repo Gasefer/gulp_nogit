37a6585eeed70472dd11cd61c5791ddbb29f3257
Oleh <108194510+Gasefer@users.noreply.github.com>
2024-04-04 10:55:04 +0300

    list_catalog

--- gulp_nogit/src/js/partials/swiper.js
+++ gulp_nogit/src/js/partials/swiper.js
@@ -1,5 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".my_swiper", {
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
